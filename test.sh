25ca7933148f8b933df785e2ae714d3c8801520f
Randy <randomzakya@gmail.com>
2023-10-25 12:36:25 +0700

    add gs.sh

--- test.sh
+++
@@ -1,3 +0,0 @@
-python ./src/train.py --config_path ./config/example.json --scaling_option standard --input_csv ./example.csv --output_dir ./out
-python ./src/eval.py --input_csv ./out/data_with_clusters.csv --output_file ./out/result.txt
-python ./src/pca.py --dataset_path ./out/data_with_clusters.csv --output_dir ./out
