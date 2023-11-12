a71f4540cbb5a3e1fb610ff3b32328cb1ae66265
rdyzakya <randomzakya@gmail.com>
2023-11-12 21:15:16 +0700

    feat select change columns argument flag

--- src/feat_select.py
+++ src/feat_select.py
@@ -17,11 +17,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Remove specified columns from a CSV file.')
     parser.add_argument('--input_csv', type=str, help='Path to input CSV file')
     parser.add_argument('--output_csv', type=str, help='Path to save the modified CSV file')
-    parser.add_argument('--columns_json', type=str, help='Path to JSON file containing columns to remove')
+    parser.add_argument('--columns', nargs='+', help='Columns to remove')
     args = parser.parse_args()
 
     # Load column names to remove from JSON file
-    with open(args.columns_json, 'r') as json_file:
-        columns_to_remove = json.load(json_file)
+    # with open(args.columns_json, 'r') as json_file:
+    #     columns_to_remove = json.load(json_file)
 
-    remove_columns(args.input_csv, args.output_csv, columns_to_remove)
+    remove_columns(args.input_csv, args.output_csv, args.columns)
