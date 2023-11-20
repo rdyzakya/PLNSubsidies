e5ae6092702ab09ffbcd7ee2a2c080d71b46b9da
Randy <randomzakya@gmail.com>
2023-11-20 16:53:54 +0700

    add visualization without cluster at pca

--- src/pca.py
+++ src/pca.py
@@ -39,25 +39,31 @@ def get_color(index):
     return random.choice([mc1, mc2])
 
 
-def save_scatter(df, out_path):
+def save_scatter(df, label_column, without_label, out_path):
     # Determine the number of clusters from the unique values in 'Cluster' column
 
     # Create a list of unique cluster labels
-    cluster_labels = df['Cluster'].unique()
+    cluster_labels = df[label_column].unique()
 
     # Create subplots
     fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 5))
-    for label in cluster_labels:
-        cluster_data = df[df['Cluster'] == label]
-        axes[0].scatter(cluster_data['PC1'], cluster_data['PC2'], color=get_color(label), label=f'Cluster {label}')
+    if not without_label:
+        for i, label in enumerate(cluster_labels):
+            cluster_data = df[df[label_column] == label]
+            axes[0].scatter(cluster_data['PC1'], cluster_data['PC2'], color=get_color(i), label=f'{label_column} {label}')
+    else:
+        axes[0].scatter(df['PC1'], df['PC2'], color=get_color(0))
     
     axes[0].set_xlabel('PC1')
     axes[0].set_ylabel('PC2')
 
     ax = fig.add_subplot(122, projection='3d')
-    for label in cluster_labels:
-        cluster_data = df[df['Cluster'] == label]
-        ax.scatter(cluster_data['PC1'], cluster_data['PC2'], cluster_data['PC3'], color=get_color(label), label=f'Cluster {label}')
+    if not without_label:
+        for i, label in enumerate(cluster_labels):
+            cluster_data = df[df[label_column] == label]
+            ax.scatter(cluster_data['PC1'], cluster_data['PC2'], cluster_data['PC3'], color=get_color(i), label=f'{label_column} {label}')
+    else:
+        ax.scatter(df['PC1'], df['PC2'], df['PC3'], color=get_color(0))
     
     ax.set_xlabel('PC1')
     ax.set_ylabel('PC2')
@@ -67,7 +73,8 @@ def save_scatter(df, out_path):
     axes[0].set_title('2D Scatter Plot (PC1 vs PC2)')
     ax.set_title('3D Scatter Plot (PC1 vs PC2 vs PC3)')
 
-    plt.legend()
+    if not without_label:
+        plt.legend()
 
     # Display the plot
     plt.tight_layout()
@@ -75,7 +82,7 @@ def save_scatter(df, out_path):
     # Save the plot to a JPG file
     plt.savefig(out_path)
 
-def perform_pca(dataset_path, label_column, pca_params, output_dir):
+def perform_pca(dataset_path, label_column, without_label, pca_params, output_dir):
     # Load dataset
     data = pd.read_csv(dataset_path)
 
@@ -90,7 +97,7 @@ def perform_pca(dataset_path, label_column, pca_params, output_dir):
     # Save principal components
     principal_components = pd.DataFrame(principal_components, columns=['PC1', 'PC2', 'PC3'])
     principal_components["Cluster"] = label_or_cluster
-    save_scatter(principal_components, os.path.join(output_dir, "scatter.jpg"))
+    save_scatter(principal_components, label_column, without_label, os.path.join(output_dir, "scatter.jpg"))
 
 
     # Correlation analysis
@@ -112,7 +119,7 @@ if __name__ == "__main__":
     parser.add_argument('--dataset_path', type=str, help='Path to the input dataset CSV file')
     parser.add_argument('--label_column', type=str, help='Column name for cluster or label (to exclude from features)', default="Cluster")
     parser.add_argument('--pca_params', type=str, help='Path to JSON file containing PCA parameters')
-    parser.add_argument('--with_label', action='store_true', help='If true, then the visualization will differ for each cluster')
+    parser.add_argument('--without_label', action='store_true', help='If true, then the visualization will not differ for each cluster (and no legend)')
     parser.add_argument('--output_dir', type=str, help='Path to output directory')
     args = parser.parse_args()
 
@@ -122,4 +129,4 @@ if __name__ == "__main__":
         with open(args.pca_params, 'r') as json_file:
             pca_params = json.load(json_file)
 
-    perform_pca(args.dataset_path, args.label_column, pca_params, args.output_dir, args.with_label)
+    perform_pca(args.dataset_path, args.label_column, args.without_label, pca_params, args.output_dir)
